7f0181cca396d22d5aa233fe4460929dc95b5849
Andres Bastidas <andres@bastidas.us>
2025-03-08 01:12:59 -0500

    Update TaskInterface.php

--- Runners/app/Interfaces/TaskInterface.php
+++ Runners/app/Interfaces/TaskInterface.php
@@ -2,6 +2,8 @@
 
 namespace App\Interfaces;
 
+// TODO: move this plus the Traits and the base library to "Shared" module so the other modules dont couple themselves with the Host
+
 interface TaskInterface
 {
     public function execute(): void;
